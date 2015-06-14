5088586fd27e503d0bb1c6587007a21682bba2ea
Connor Weng <foamtea30@gmail.com>
2015-06-14 23:38:32 +0800

    initial commit

---
+++ src/main/java/com/zwd51/messager/MessageDispatcher.java
@@ -0,0 +1,15 @@
+package com.zwd51.messager;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by Connor on 6/14/15.
+ */
+public class MessageDispatcher extends HttpServlet {
+
+    public MessageDispatcher() {
+        super();
+        System.out.println("i am message dispatcher");
+    }
+
+}
